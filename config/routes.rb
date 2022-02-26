41f0f5aa532947bdd8a58f5aafed517caf1bc1f1
skfy <shiro218266@gmail.com>
2022-02-26 14:11:03 +0000

    退会機能の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root to: 'homes#top'
-  #resources :users
-  resources :users do
-    resources :likes, only: [:create, :destroy]
+  #resources :users do
+    #resources :likes, only: [:create, :destroy]
     #member do
       #get :likes
     #end
-  end
-  resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+  #end
+  resources :posts do
     resource :likes, only: [:index, :show, :create, :destroy]
+    
     resources :comments, only: [:create, :destroy]
   end
-  
-  get 'users/like' => 'users#like'
+
+  #get 'users/like' => 'users#like'
   get 'users/my_page' => 'users#show'
   get 'users/unsubscribe' => 'users#unsubscribe'
-  patch 'users/withdraw' => 'users#withdraw'
+  #patch 'users/withdraw' => 'users#withdraw'
+  get 'smoking_informations' => 'smoking_informations#index'
   #put 'withdraw/:id' => 'users#withdraw'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :smoking_informations
+  #resources :smoking_informations
 
 end
