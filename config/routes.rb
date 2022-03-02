1f9b98e43b3c49913645ac70dd8133612490bc0c
skfy <shiro218266@gmail.com>
2022-03-02 12:56:35 +0000

    GoogleAPI導入

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #resources :smoking_informations
-
+  resources :maps, only: [:index]
 end
