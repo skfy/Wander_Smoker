4e32e8c4d5865fac9da302391fcd379151b607e6
skfy <shiro218266@gmail.com>
2022-04-18 23:15:58 +0000

    マイ喫煙情報一覧画面　機能修正・レイアウト調整

--- app/controllers/smoking_informations_controller.rb
+++ app/controllers/smoking_informations_controller.rb
@@ -1,8 +1,27 @@
 class SmokingInformationsController < ApplicationController
-  def index
-    @user = current_user
-    #@smoking_information = SmokingInformation.find(params[:id])
+  def new
+    @smoking_information = SmokingInformation.new
   end
+  def create
+    @smoking_information = SmokingInformation.new(smoking_information_params)
+    @smoking_information.user_id = current_user.id
+    @smoking_information.save
+    redirect_to smoking_informations_path
+  end
+  def index
+    @smoking_information = SmokingInformation.new
+    @smoking_informations = SmokingInformation.page(params[:page]).reverse_order
+    #@smoking_information = SmokingInformation.find(params[:id])
+    #@user = current_user
+    #@smoking_informations = @user.smoking_information
+  end
+
+
+  #def index
+    #@user = current_user
+    #@smoking_informations = @user.smoking_information
+    #@smoking_information = SmokingInformation.find(params[:id])
+  #end
   def show
     
   end
@@ -14,4 +33,11 @@ class SmokingInformationsController < ApplicationController
   def destroy
 
   end
+  
+  private
+
+  def smoking_information_params
+    params.require(:smoking_information).permit(:user_id, :total_number, :total_amount, :no_smoking_days, :tax_payment_amount)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,20 +16,20 @@ Rails.application.routes.draw do
       get :likes
     end
   end
-  
-  resources :posts, only: [:new, :create, :index, :show, :edit, :destroy] do
+
+  resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resource :likes, only: [:index, :show, :create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
 
   #get 'users/like' => 'users#like'
   #get 'smoking_informations' => 'smoking_informations#index'
-  resources :smoking_informations, only: [:show, :index, :update, :destroy]
+  resources :smoking_informations, only: [:new, :create, :show, :index, :update, :destroy]
   #patch 'smoking_informations/index' => 'smoking_informations#index'
   #put 'withdraw/:id' => 'users#withdraw'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #resources :smoking_informations
   resources :maps, only: [:index]
-  
+
 end
