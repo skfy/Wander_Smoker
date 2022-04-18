1067380d633d59e5a9d57f4d0ac9af6d970feae2
skfy <shiro218266@gmail.com>
2022-04-18 14:26:59 +0000

    コメント機能修正・レイアウト調整

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,10 +25,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    #@user = User.find(params[:id])
     @post = Post.find(params[:id])
+    @comments = @post.comments
     @comment = Comment.new
-    
+    #@user = User.find(params[:id])
+    #@post = Post.find(params[:id])
+    #@comment = Comment.new
   end
 
   def edit
