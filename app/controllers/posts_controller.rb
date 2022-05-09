003aa41db4e6533192e7aa68105c6f75cfa10085
skfy <shiro218266@gmail.com>
2022-05-09 14:56:52 +0000

    権限・バリデーションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,15 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    @post.save
-    redirect_to posts_path
+    #@post.save
+    #redirect_to posts_path
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+      #render :index
+      #redirect_to posts_path
+    end
   end
 
   def index
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,10 @@ class Post < ApplicationRecord
  has_one :spot, dependent: :destroy
   accepts_nested_attributes_for :spot
 
+ validates :title, presence: true
+ validates :comment, presence: true
+ validates :image, presence: true
+
  def liked_by?(user)
    likes.where(user_id: user.id).exists?
  end
