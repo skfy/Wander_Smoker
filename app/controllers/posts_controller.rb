8d7dec8b190bb3fff1e4fc7b6f9eacdc76681738
skfy <shiro218266@gmail.com>
2022-05-10 11:34:11 +0000

    ransack　検索機能修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,9 +35,10 @@ class PostsController < ApplicationController
     #@post = Review.new
     @post.build_spot
     #@posts = Post.all
-    #@posts = Post.page(params[:page]).reverse_order
     # distinct: trueは重複したデータを除外
+    @q = Post.ransack(params[:q])
     @posts = @q.result(distinct: true)
+    @posts = Post.page(params[:page]).reverse_order
   end
 
   def show
