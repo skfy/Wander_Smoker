6d6a4d6195c9c6357e72f06dacacd18aa64a4788
skfy <shiro218266@gmail.com>
2022-06-22 14:09:22 +0000

    migrate 更新3

--- db/migrate/20220227230626_add_tax_payment_amount_to_smoking_information.rb
+++ db/migrate/20220227230626_add_tax_payment_amount_to_smoking_information.rb
@@ -1,5 +1,5 @@
 class AddTaxPaymentAmountToSmokingInformation < ActiveRecord::Migration[5.2]
   def change
-    add_column :smoking_informations, :tax_payment_amount, :integer
+    change_column :smoking_informations, :tax_payment_amount, :integer
   end
 end
